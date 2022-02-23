3b1138d48682859dfd13b9fc251ac0324f0d4f0a
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-02-23 18:49:03 +0000

    Add structure of the functions for the game

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAddQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+
+}
